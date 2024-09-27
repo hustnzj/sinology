1517476fa3258719ca2dfc26606a3819d2740d57
hustnzj <hustnzj@users.noreply.github.com>
2024-09-27 05:18:57 +0000

    deploy: hustnzj/sinology_learning@fc07401e9ec8b7f25de358f1808dd7ebc91e0d36

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
 from setuptools import setup
 
 setup(
-    name='zj_resize_png',
-    version='1.0',
-    py_modules=['_py.batch_resize_png',
+    name='note_system_python_tools',
+    version='1.1',
+    py_modules=['_py.batch_resize_imgs',
                 '_py.convert_html_to_md',
                 '_py.convert_footnotes',
                 '_py.word_pinyin',
@@ -12,7 +12,7 @@ setup(
                 ],
     entry_points={
         'console_scripts': [
-            'resize_png = _py.batch_resize_png:main',
+            'resize_imgs = _py.batch_resize_imgs:main',
             'html2md = _py.convert_html_to_md:main',
             'ask_footnotes = _py.convert_footnotes:main',
             'word_pinyin = _py.word_pinyin:main',
